556b0e670964b42e011383e339322bb86fce8597
JJ Ding <dgdunix@gmail.com>
2011-09-06 22:35:31 +0800

    Do not show menubar
    Also do those mouse interface things as emacs-starter-kit does.

--- init.el
+++ init.el
@@ -1,10 +1,13 @@
 ;; dgdunix's emacs init file
 
+; do not show menu bar
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
 ; do not show toolbar
-(tool-bar-mode -1)
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 
 ; do not use scrollbar
-(scroll-bar-mode -1)
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 ; really kill message buffer after sending mail
 (setq message-kill-buffer-on-exit t)
